9ac53373b64cd54b9475ce62e7fb4c5d547078cc
Hardi99 <harditabuna@gmail.com>
2023-08-31 15:33:15 +0200

    first commit

---
+++ src/pages/Characters.jsx
@@ -0,0 +1,45 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+const Characters = () => {
+  const [data, setData] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get("http://localhost:3000/characters");
+        // console.log(response.data);
+        setData(response.data);
+        setIsLoading(false);
+      } catch (error) {
+        console.log(error.message);
+        console.log(error.response);
+      }
+    };
+    fetchData();
+  }, []);
+
+  return isLoading ? (
+    <p>Loading ...</p>
+  ) : (
+    <div>
+      {data.results.map((character) => {
+        return (
+          <article key={character._id}>
+            <h2>{character.name}</h2>
+            <p>{character.description}</p>
+            <img
+              src={
+                character.thumbnail.path + "." + character.thumbnail.extension
+              }
+              alt=""
+            />
+          </article>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Characters;
